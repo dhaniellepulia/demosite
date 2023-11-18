1c9c6e8bab0bcb0af6d1069b7bc3f26161f3b2dc
dhanpulia <dhan.pulia@latisglobal.net>
2023-11-18 10:19:03 +0800

    first files

---
+++ data.js
@@ -0,0 +1,177 @@
+var siteData = {
+  gallery_photos: {
+    "Image 1": "/assets/gallery/1.jpg",
+    "Image 2": "/assets/gallery/2.jpg",
+    "Image 3": "/assets/gallery/3.jpg",
+    "Image 4": "/assets/gallery/4.jpg",
+    "Image 5": "/assets/gallery/5-1.jpg",
+    "Image 6": "/assets/gallery/5-2.jpg",
+    "Image 7": "/assets/gallery/6.jpg",
+    "Image 8": "/assets/gallery/7.jpg",
+    "Image 9": "/assets/gallery/8.jpg",
+    "Image 10": "/assets/gallery/9-1.jpg",
+    "Image 11": "/assets/gallery/9-2.jpg",
+    "Image 12": "/assets/gallery/10-1.jpg",
+    "Image 13": "/assets/gallery/10-2.jpg",
+    "Image 14": "/assets/gallery/11.png",
+    "Image 15": "/assets/gallery/12-1.jpg",
+    "Image 16": "/assets/gallery/12-2.jpg",
+  },
+  grain_dryer: {
+    "Image 1": "/assets/products-main-images/grain-dryer-photo2.jpg",
+    "Image 3": "/assets/products-main-images/grain-dryer-photo3.jpg",
+  },
+  bulk_dryer: {
+    "Image 1": "/assets/products-main-images/bulk-dryer1.jpg",
+    "Image 2": "/assets/products-main-images/bulk-dryer2.jpg",
+    "Image 3": "/assets/products-main-images/bulk-dryer3.jpg",
+    "Image 4": "/assets/products-main-images/bulk-dryer4.jpg",
+    "Image 5": "/assets/products-main-images/bulk-dryer5.jpg",
+    "Image 6": "/assets/products-main-images/bulk-dryer6.jpg",
+    "Image 7": "/assets/products-main-images/bulk-dryer-photo2.jpg",
+    "Image 8": "/assets/products-main-images/bulk-dryer-photo3.jpg",
+    "Image 9": "/assets/products-main-images/bulk-dryer-photo4.jpg",
+  },
+  rice_huller: {
+    "Image 1": "/assets/products-main-images/rice-huller-photo1.jpg",
+  },
+  husk_blower: {
+    "Image 1": "/assets/products-main-images/husk-blower-photo1.jpg",
+  },
+  paddy_separator: {
+    "Image 1": "/assets/products-main-images/paddy-separator-photo1.jpg",
+  },
+  de_stoner: {
+    "Image 1": "/assets/products-main-images/de-stoner-photo1.jpg",
+  },
+  vertical_whitener: {
+    "Image 1": "/assets/products-main-images/vertical-whitener-photo1.jpg",
+  },
+  rotary_sifter: {
+    "Image 1": "/assets/products-main-images/rotary-sifter-photo1.jpg",
+  },
+  mist_polisher: {
+    "Image 1": "/assets/products-main-images/mist-polisher-photo1.jpg",
+  },
+  color_sorter: {
+    "Image 1": "/assets/products-main-images/color-sorter-photo1.jpg",
+    "Image 2": "/assets/products-main-images/color-sorter-photo2.jpg",
+    "Image 3": "/assets/products-main-images/color-sorter-photo3.jpg",
+    "Image 4": "/assets/products-main-images/color-sorter-photo4.jpg",
+    "Image 5": "/assets/products-main-images/color-sorter-photo5.jpg",
+    "Image 6": "/assets/products-main-images/color-sorter-photo6.jpg",
+    "Image 7": "/assets/products-main-images/color-sorter-photo7.jpg",
+  },
+  mobile_ricemill: {
+    "Image 1": "/assets/products-main-images/mobile-ricemill1.jpg",
+    "Image 4": "/assets/products-main-images/mobile-ricemill-photo2.jpg",
+    "Image 5": "/assets/products-main-images/mobile-ricemill-photo3.jpg",
+  },
+};
+
+$(document).ready(function () {
+  $.each(siteData.gallery_photos, function (k, imageSrc) {
+    var galleryItem = $("<div class='gallery-item'></div>");
+    galleryItem.append($("<img>", { src: imageSrc }));
+    $(".gallery-content-slider").append(galleryItem);
+  });
+
+  $.each(siteData.grain_dryer, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".grain-dryer .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".grain-dryer .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.bulk_dryer, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".bulk-dryer .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".bulk-dryer .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.rice_huller, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".rice-huller .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".rice-huller .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.husk_blower, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".husk-blower .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".husk-blower .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.paddy_separator, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".paddy-separator .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".paddy-separator .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.de_stoner, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".de-stoner .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".de-stoner .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.vertical_whitener, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".vertical-whitener .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".vertical-whitener .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.rotary_sifter, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".rotary-sifter .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".rotary-sifter .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.mist_polisher, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".mist-polisher .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".mist-polisher .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.color_sorter, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".color-sorter .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".color-sorter .thumbnail-slider").append(thumbnailDiv);
+  });
+
+  $.each(siteData.mobile_ricemill, function (k, imageSrc) {
+    var productPhoto = $("<img>", { src: imageSrc });
+    $(".mobile-ricemill .product-photo").append(productPhoto);
+
+    var thumbnailDiv = $("<div class='thumbnail-item'></div>");
+    thumbnailDiv.append($("<img>", { src: imageSrc }));
+    $(".mobile-ricemill .thumbnail-slider").append(thumbnailDiv);
+  });
+});
---
+++ script.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+  $(".banner-carousel").slick({
+    dots: true,
+    autoplay: true,
+  });
+
+  $(".tab-item[data-category='all']").addClass("active");
+
+  $(".tab-item").click(function () {
+    $(".tab-item").removeClass("active");
+
+    $(this).addClass("active");
+
+    var selectedCategory = $(this).data("category");
+
+    $(".homepage-product-item").each(function () {
+      var productCategory = $(this).data("category");
+      if (selectedCategory === "all" || selectedCategory === productCategory) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+
+  $(".gallery-content-slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1000, // Adjust as needed
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768, // Adjust as needed
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $(".product-photo").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: ".thumbnail-slider",
+  });
+
+  // Initialize the thumbnail slider
+  $(".thumbnail-slider").slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".product-photo",
+    focusOnSelect: true,
+  });
+  const thumbnailSlider = $(".thumbnail-slider");
+  if (thumbnailSlider.find(".thumbnail-item").length <= 1) {
+    thumbnailSlider.css("display", "none");
+  }
+});
+
+// Get all anchor elements on the page
+const anchorElements = document.querySelectorAll("a");
+
+// Loop through each anchor element
+anchorElements.forEach((anchor) => {
+  const href = anchor.getAttribute("href");
+
+  // Check if the href attribute contains ".html"
+  if (href && href.includes(".html")) {
+    // Remove ".html" from the href attribute
+    anchor.setAttribute("href", href.replace(".html", ""));
+  }
+});
+
+function openMenu() {
+  var x = document.getElementById("mobileMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+const div = document.getElementById("clickWrapper");
+
+div.addEventListener("click", () => {
+  if (div.childNodes[0].classList.contains("close")) {
+    div.childNodes[0].className = "menu animate";
+  } else {
+    div.childNodes[0].className = "close animate";
+  }
+});
+
+var acc = document.getElementsByClassName("products-dropdown");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = document.querySelector(".mobile-dropdown-content");
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+Fancybox.bind('[data-fancybox="gallery"]', {});
